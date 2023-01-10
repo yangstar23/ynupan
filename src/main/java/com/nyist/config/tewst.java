9e9bda684be6d3d6180f64e3056c5f344e779ca0
yangstar <goodmale@qq.com>
2023-01-10 23:31:27 +0800

    项目提交

---
+++ src/main/java/com/nyist/config/tewst.java
@@ -0,0 +1,18 @@
+package com.nyist.config;
+
+import java.math.BigDecimal;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class tewst {
+    public static void main(String[] args) throws ParseException {
+        String a = "";
+        List<String> lits = new ArrayList<>();
+        lits.add("video");
+        lits.add(a);
+        System.out.println(lits);
+    }
+}
---
+++ src/test/java/com/nyist/SpringbootFilemanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nyist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootFilemanagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
